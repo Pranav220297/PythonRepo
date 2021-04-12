90dcc8fe4cbeb3883eaa64d6708fc2326f150f39
Pranav Kumar <pranavkrcmr@gmail.com>
2021-04-12 10:19:31 +0530

    db fie commited

--- db.py
+++ db.py
@@ -1,23 +1,26 @@
 import mysql.connector
 
-
-cursor = db.cursor()
-
-def verifyUser():
-    id =input("Pls enter id")
-    passw =input("Pls enter password")
-
+def verifyUser(uid, passw):
+    
     db = mysql.connector.connect(
       host="localhost",
       user="root",
       password="root",
       database = "mydb")
 
-    info = cursor.execute("SELECT USERID , PASSWORD FROM USER WHERE USERID = id ")
-    print(info)
+    cursor = db.cursor()
+    sql = "SELECT * FROM user WHERE userid ='" +uid+"'"
+    cursor.execute(sql)
+    info = cursor.fetchone()
+    
+    if(passw == info[1]):
+        return True
+    else:
+        return False
+     
+output = verifyUser('def','45')
+print(output)
 
-
-verifyUser()
     
 
 
